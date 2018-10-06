bda8547266d6cb27eb0714276b6e37ab5e273e53
Alina Kitaeva <akitaeva@gmail.com>
2018-10-06 11:21:09 -0400

    add button event logic

--- WebDev/score-keeper/script.js
+++ WebDev/score-keeper/script.js
@@ -1,14 +1,54 @@
-let p1score = Number(document.getElementById("p1").innerHTML);
-let p2score = Number(document.getElementById("p2").innerHTML);
+let p1score = 0;
+let p1display = document.querySelector("#p1display");
+let p2score = 0;
+let p2display = document.querySelector("#p2display");
 const buttonP1 = document.querySelector("#player1");
 const buttonP2 = document.querySelector("#player2");
+const buttonRe = document.querySelector("#reset");
+const scoreInput = document.getElementById("score") 
+const scoreDisplay = document.getElementById("scoreDisplay").textContent
+
 
 buttonP1.addEventListener("click", ()=> {
-  p1score +=1; 
-  console.log(p1score, p2score)
+  if (p1score < Number(scoreDisplay)) 
+  {
+      p1score++; 
+      p1display.textContent = p1score;
+  } else {
+      p1display.classList.toggle("winner");
+      alert("Player 1 wins!")
+      p1score =0;
+      p1display.textContent = p1score;
+      p2score =0;
+      p2display.textContent = p2score;
+  }
 })
 
 buttonP2.addEventListener("click", ()=> {
-    p2score +=1; 
-    console.log(p1score, p2score)
+   if (p2score < Number(scoreDisplay) ) 
+   {
+       p2score++; 
+       p2display.textContent = p2score;
+   } else 
+   {
+      p2display.classList.toggle("winner");
+      alert("Player 2 wins!")
+      p1score =0;
+      p1display.textContent = p1score;
+      p2score =0;
+      p2display.textContent = p2score;
+}
   })
+
+buttonRe.addEventListener("click", ()=> {
+    if (window.confirm("Are you sure you want to reset the score?")) {
+        p1score =0;
+        p1display.textContent = p1score;
+        p2score =0;
+        p2display.textContent = p2score;
+    }
+})
+
+scoreInput.addEventListener("change", ()=>{
+    document.querySelector("#scoreDisplay").textContent = scoreInput.value;
+})
