0d17565801fbed941e333a27fee19e5ab2ea5321
Alina Kitaeva <akitaeva@gmail.com>
2018-10-09 13:26:33 -0400

    add reset button logic

--- WebDev/color-game/script.js
+++ WebDev/color-game/script.js
@@ -1,37 +1,55 @@
-
-
-let pickedColorDisplay = document.getElementById("pickedColor")
+let pickedColorDisplay = document.getElementById("pickedColor");
 const messageDisplay = document.querySelector("#message");
 const squares = document.querySelectorAll(".square");
+const heading = document.querySelector("#heading");
+const reset = document.querySelector("#reset");
+const level = document.querySelector("#level");
 
 
 const setField = () => {
+   //getting array of colors according to the difficulty level 
    let colors = fillInColors(6);
+   //chosing the color to be guessed
    let pickedColor = colors[Math.floor(Math.random() * colors.length)];
-   console.log("colors here: ", colors)
-    //add intial colors to sqaures
+   console.log("pickedColor in setField ", pickedColor)
+   //displaying the color to guess in the HTML doc
+   pickedColorDisplay.textContent = pickedColor;
+   //changing the heading background to default
+   heading.style.backgroundColor = "#2b3c53";
+   //add the colors to sqaures
    squares.forEach((oneSquare, i) => {
     oneSquare.style.backgroundColor = colors[i]
    })
-   //add click listeners to squares
-   squares.forEach((oneSquare, i) => {
-    oneSquare.addEventListener("click", ()=>{
-    let clickedColor = oneSquare.style.backgroundColor;   
-     if (clickedColor === pickedColor) {
-         messageDisplay.textContent = "Correct!";
-         changColors();
-        }  
-     else {
-        messageDisplay.textContent = "Try again!";
-        oneSquare.style.backgroundColor = "#2b3c53"
-     }
-    })
+ 
+   //add "click" listeners to squares
+   squares.forEach((oneSquare) => {
+
+      oneSquare.addEventListener("click", ()=>{  
+        console.log ("are stuck in this part??", pickedColor);  
+        let clickedColor = oneSquare.style.backgroundColor;  
+          if (clickedColor === this.pickedColor.textContent) 
+           {  
+             messageDisplay.textContent = "Correct!";
+             changeColors();
+             heading.style.backgroundColor = pickedColor;
+             console.log("heading background ", heading.backgroundColor)
+           }  
+          else 
+           {
+             messageDisplay.textContent = "Try again!";
+             oneSquare.style.backgroundColor = "#2b3c53"
+             console.log ("stuck in else??", pickedColor);
+           }
+        })
+         
    })
 }
 
-const changColors = () => {
+
+const changeColors = () => {
     squares.forEach((square) =>{
-    square.style.backgroundColor = pickedColor;
+    console.log("are you here? ", pickedColor)
+    square.style.backgroundColor = pickedColor.textContent;
     })
 }
 
@@ -39,7 +57,6 @@ const fillInColors = (num) => {
    let arr = [];
    for (let i=0; i<num; i++) {
         arr.push(getRandomColor());
-        console.log(arr);
     }
     return arr;
 }
@@ -48,11 +65,23 @@ const getRandomColor = () => {
    let red = Math.floor(Math.random() * 256);
    let green = Math.floor(Math.random() * 256);
    let blue = Math.floor(Math.random() * 256)
-   console.log("rgb("+red+", "+green+", "+blue+")");
+   //what is returned ?:
+   console.log("what is returned in getRandomColor: rgb("+red+", "+green+", "+blue+")");
    return ("rgb("+red+", "+green+", "+blue+")");
-   // "rgb(0, 176, 214)",
+
    
 }
 
 setField();
-pickedColorDisplay.textContent = pickedColor;
+
+reset.addEventListener("click", ()=> {
+    if (prompt("Would you like to start a new game? ")) {
+        setField();
+    }
+  
+})
+
+level.addEventListener("click", ()=> {
+
+
+})
