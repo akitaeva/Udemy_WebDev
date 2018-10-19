c23d3e2db69ec7fedcfe0f6ce2442c49ed5d891a
Alina Kitaeva <akitaeva@gmail.com>
2018-10-18 23:59:01 -0400

    add search form

--- WebDev/backend/movie-search-app/app.js
+++ WebDev/backend/movie-search-app/app.js
@@ -2,15 +2,18 @@ const express = require("express");
 const app = express();
 const request = require("request")
 
-app.use(express.static("public"))
+
 app.set('view engine', 'ejs');
 
+app.get("/", function(req, res) {
+    res.render("search")
+})    
 
 app.get("/results", function(req, res) {
     request("http://www.omdbapi.com/?s=idaho&apikey=thewdb", (error, response, body) => {
         if (!error && response.statusCode == 200) {
-            var results = JSON.parse(body);
-            res.send(results["Search"][0]);
+            var data = JSON.parse(body);
+            res.render("results", {data: data});
         }
     })
 })
