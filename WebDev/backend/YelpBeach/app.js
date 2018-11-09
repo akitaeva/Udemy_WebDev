05d0cf3294322e16bf18457bd94bb731dc399285
Alina Kitaeva <akitaeva@gmail.com>
2018-11-09 00:44:03 -0500

    add comments routes

--- WebDev/backend/YelpBeach/app.js
+++ WebDev/backend/YelpBeach/app.js
@@ -3,7 +3,8 @@ const app = express();
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
 
-const Beach = require("./models/beach")
+const Beach = require("./models/beach");
+const Comment = require("./models/comment")
 
 
 //Connect to the DB
@@ -23,7 +24,7 @@ app.get("/beaches", (req,res) =>  {
         if(err) {
             console.log(err);
         } else {
-            res.render("index", { theBeaches : allBeaches})
+            res.render("beaches/index", { theBeaches : allBeaches})
         }
 
     });
@@ -45,20 +46,39 @@ app.post("/beaches", (req,res) => {
 })
 
 app.get("/beaches/new", (req,res) => {
-    res.render("addNew.ejs");
+    res.render("beaches/new");
 })
 
 //SHOW - details about a specific beach - find by id and render the template
 app.get("/beaches/:id", (req,res) => {
-    Beach.findById(req.params.id, (err, foundBeach)=> {
+    //find the beach by the provided id
+    Beach.findById(req.params.id)
+    .populate("comments")
+    .exec((err, foundBeach)=> {
        if (err) {
            console.log(err);
        } else {
-        res.render("beachDetails", {theBeach: foundBeach});
+        res.render("beaches/show", {theBeach: foundBeach});
        }
     });
     
 })
+
+//========================================
+//       COMMENTS ROUTES
+//========================================
+
+app.get("/beaches/:id/comments/new", (req, res) =>{
+    //find beach by id 
+    Beach.findById(req.params.id, function(err, foundBeach) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", {theBeach: foundBeach});
+        }
+    })
+
+});
 
 //start the server
 app.listen(3000, () => {
--- WebDev/backend/YelpBeach/models/beach.js
+++ WebDev/backend/YelpBeach/models/beach.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
 
+
 //Set up a schema for the beach entry
 const beachSchema = new mongoose.Schema ({
     name: String,
     image: String,
     description: String,
+    comments: [
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
 })
 
 //Compile schema into the model and export it
