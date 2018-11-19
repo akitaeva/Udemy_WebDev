a4aa3ad0e4e67546794744c9aae759cb1e04b14a
Alina Kitaeva <akitaeva@gmail.com>
2018-11-19 16:36:58 -0500

    add user model

--- WebDev/backend/AuthPractice/app.js
+++ WebDev/backend/AuthPractice/app.js
@@ -1,8 +1,9 @@
 const express               = require("express"),
       mongoose              = require("mongoose"),
-      passport              = require("passport"),
+      passport              = require("passport"),                 
       bodyParser            = require("body-parser"),
-      LocalStrategy         = require("passport-local"),
+      User                  = require("./models/user")
+      LocalStrategy         = require("passport-local"),         
       passportLocalMongoose = require("passport-local-mongoose");
 
 
@@ -11,10 +12,13 @@ mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true })
 const app         = express();
 app.set("view engine", "ejs");
 
+
+//main page route
 app.get("/", (req, res) => {
     res.render("home");
 });
 
+//restricted route
 app.get("/secret", (req, res) => {
     res.render("secret");
 });
