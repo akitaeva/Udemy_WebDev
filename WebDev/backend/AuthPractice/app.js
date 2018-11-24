55d27918d1ced64b6323f035dd376b575d7b68ed
Alina Kitaeva <akitaeva@gmail.com>
2018-11-23 23:14:35 -0500

    add login view and logic

--- WebDev/backend/AuthPractice/app.js
+++ WebDev/backend/AuthPractice/app.js
@@ -27,6 +27,8 @@ app.use(require("express-session")({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new LocalStrategy(User.authenticate()));
+
 //encoding and decoding data methods for the session
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -59,17 +61,32 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
     req.body.username
     req.body.password
-    User.register(new User({username: req.body.username}), req.body.password, (err, user) => { 
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) { 
         if (err) {
           console.log(err);
-          return res.render("register");
+          return res.render("register", {message: "Something went wrong =[ " });
         }
-        passport.authenticate("local")( (req, res, () => {
+
+        passport.authenticate("local")( req, res, () => {
             res.redirect("/secret");
-        }));
+        });
     });
 });
 
+// LOGIN ROUTES
+//render login form
+app.get("/login", (req, res) => {
+    res.render("login");
+
+})
+
+//login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), (req, res) => {
+
+});
 
 app.listen(3000, ()=> {
     console.log("Server is up and running on port 3000")
